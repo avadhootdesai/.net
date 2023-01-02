06a1401d87b88a5267e629bf9eb22ed79f9120d0
Avadhoot Desai <83361724+avadhootdesai@users.noreply.github.com>
2023-01-02 13:41:47 +0530

    Add files via upload
    fill grid view with binding html grid view columns with sql column names, auto generate columns = false.
    total of price integer data in footer of grid view

---
+++ draft.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="draft.aspx.cs" Inherits="draft" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+
+     <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous" />
+    <title>Anti-Report BCHT</title>
+    <link rel = "icon" href = "css/bch_title_logo.png" type = "image/x-icon" />
+
+</head>
+
+<body>
+
+    <form id="form1" runat="server">
+    <div>
+         <img class ="image" alt="" src="css/bcht_index_logo.png"/>
+         <br />
+    <center>
+          
+          <asp:Label class="label" style="color:Red;" ID="alert" runat="server" visible="false" /><br />
+
+          <asp:Label class="label" ID="label1" runat="server" Text="Admission No:"/> &nbsp
+          <asp:TextBox ID="ad_no" runat="server" autocomplete="off"></asp:TextBox>
+          &nbsp &nbsp
+
+          <asp:Button class="btn btn-success" ID="getReport" runat="server" Text="Generate Report" 
+           onclick="getReport_Click" /> &nbsp &nbsp &nbsp
+          <asp:Button class="btn btn-success" ID="exportReport" runat="server" Text="Export To Excel" 
+           onclick="exportReport_Click" />
+
+          <br /><br />
+
+         <div class="gridViewwDiv" style="overflow:auto; height:16.4cm;">
+         <asp:GridView class="gridTable" ID="draftReportTable" runat="server" ShowFooter="true" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="MRNO" HeaderText="MRN_NO"  />
+                <asp:BoundField DataField="ADMISSIONNO" HeaderText="ADMISSION_NO"  />
+                <asp:BoundField DataField="PATIENTNAME" HeaderText="PATIENT_NAME"  />
+                <asp:BoundField DataField="FINANCEGROUP" HeaderText="FIANACE_GROUP"  />
+                <asp:BoundField DataField="SERVICE" HeaderText="SERVICE_CHARGE"  />
+            </Columns>
+         </asp:GridView>
+         </div>
+
+     </center>
+    </div>
+    </form>
+
+</body>
+</html>
---
+++ draft.aspx.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Drawing;
+
+public partial class draft : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void getReport_Click(object sender, EventArgs e)
+    {
+        alert.Visible = false;
+        string admission_no = ad_no.Text.ToString();
+
+        if (admission_no == "") {
+
+            alert.Text = "Please Enter Admission No.";
+            alert.Visible = true;
+        }
+
+        try
+        {
+            OleDbConnection con = new OleDbConnection(ConfigurationManager.ConnectionStrings["ConnectionString2"].ConnectionString);
+            string query = @"SELECT MRNO,ADMISSIONNO,PATIENTNAME,FINANCEGROUP,SUM(DECODE(sc.CANCELLED_BY,NULL,PATIENT_NET_AMT,-PATIENT_NET_AMT)) Service
+                            FROM PATIENT_CHARGE_ADMITTED_VW pc,serviceorder sc 
+                            WHERE ADMISSIONNO='{0}'
+                            and serviceorder_number = sc.DOCNUM
+                            and sc.service_name = pc.servicename
+                            and sc.ORDER_STATUS <> 662
+                            group by MRNO,ADMISSIONNO,PATIENTNAME,FINANCEGROUP
+                            union all
+                            SELECT MRN_NO,ADMISSIONNO,PATIENT_NAME,'Surgical / Drug / Consumables', SUM(PATIENT_NET_AMOUNT) Drug FROM XXICT_PAT_DRUG_ORDER_SUMMARY 
+                            WHERE ADMISSIONNO='{0}' 
+                            group by MRN_NO,ADMISSIONNO,PATIENT_NAME
+                            order by financegroup";
+
+            //bind date inputs with above query 
+            object[] conditions = new object[] { admission_no };
+            string datequery = string.Format(query, conditions);
+
+            //execute new query 
+            OleDbCommand command = new OleDbCommand(datequery, con);
+            con.Open();
+            DataSet gridTable = new DataSet();
+            OleDbDataAdapter da = new OleDbDataAdapter(command);
+
+            //fill dataset 'gridTable' with query results
+            da.Fill(gridTable);
+            //set gridview 'invTable' equal to dataset 'gridTable' 
+            draftReportTable.DataSource = gridTable;
+
+            draftReportTable.Columns[3].FooterText = "TOTAL";
+            double totalSalary = 0;
+            foreach (DataRow dr in gridTable.Tables[0].Rows)
+            {
+                totalSalary += Convert.ToDouble(dr["SERVICE"]);
+            }
+            draftReportTable.Columns[4].FooterText = totalSalary.ToString();
+            draftReportTable.FooterStyle.ForeColor = System.Drawing.Color.Red;
+
+            draftReportTable.DataBind();
+            //making grid visible as it is hidden on page-load
+            draftReportTable.Visible = true;
+            con.Close();
+        }
+        catch {
+            alert.Text = "No Data Found! Check Admission No.";
+            alert.Visible = true;
+        }
+
+    }
+    protected void exportReport_Click(object sender, EventArgs e)
+    {
+        try
+        {
+
+            int rowCount = draftReportTable.Rows.Count;
+            if (rowCount != 0)
+            {//if gridview is not empty - export to excel
+                Response.Clear();
+                //file name for exported excel
+                Response.AddHeader("content-disposition", "attachment; filename=Unbilled_Amount_Report.xls");
+                Response.ContentType = "application/vnd.xls";
+
+                System.IO.StringWriter stringWrite = new System.IO.StringWriter();
+                System.Web.UI.HtmlTextWriter htmlWrite = new HtmlTextWriter(stringWrite);
+                draftReportTable.RenderControl(htmlWrite);
+                Response.Write(stringWrite.ToString());
+                Response.Flush();
+                Response.End();
+            }
+            else
+            {//if gridview is empty - show alert message
+               
+                alert.Visible = true;
+                alert.Text = "Please generate report first!";
+            }
+        }
+        //if excel generation failed throw below error
+        catch
+        {
+            
+            alert.Visible = true;
+            alert.Text = "Export Failed! Contact IT Department.";
+        }
+    }
+
+    public override void VerifyRenderingInServerForm(Control control) //important
+    {
+        /* Confirms that an HtmlForm control is rendered for the specified ASP.NET
+           server control at run time. */
+    }
+}
